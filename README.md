ac2461c1ac59a5f9565502008cf603628881a92e
Raul Mangolin <eu@raulmangolin.com>
2017-01-03 22:06:34 -0200

    Now with reverter

--- README.md
+++ README.md
@@ -14,13 +14,24 @@ Using require:
     //this will print "rlWhLJ1yVwbtVyWuqJjtGJShM29fnJ4vsD=="
 ```
 
-##How to convert this data in a backend code?
-It's simple
+##How to convert this hash?
+It's simple. Just pass one more parameter _true_ and de hash will be reverted.
+
+**JavaScript**
+```javascript
+    mangoHash = require("mango-hash");
+    
+    var data = 'rlWhLJ1yVwbtVyWuqJjtGJShM29fnJ4vsD==';
+    var result = mangoHash(data, true);
+    
+    console.log(result); 
+    //this will print "Raul mangolin"
+```
+
+If you want decode this hash in another code language, try to use rot13 and base64 decode like this PHP code bellow:
 
 **PHP**
 ```php
     $hash = 'rlWhLJ1yVwbtVyWuqJjtGJShM29fnJ4vsD==';
     echo base64_decode(str_rot13($hash));
 ```
-
-I'll code a JavaScript converter soon. 
--- index.js
+++ index.js
@@ -6,20 +6,29 @@
  * @param str string
  * @return string
  */
-function mangoHash(str) {
-    function encodeText(str) {
-        return str.replace(/[a-zA-Z]/g, function (c) {
-            return String.fromCharCode((c <= "Z" ? 90 : 122) >= (c = c.charCodeAt(0) + 13) ? c : c - 26);
-        });
+function mangoHash(str, revert) {
+    if (revert) {
+        return mangoB64DecodeUnicode(mangoEncodeText(str));
     }
+    return mangoEncodeText(mangoB64EncodeUnicode(str));
+}
 
-    function b64EncodeUnicode(str) {
-        return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function (match, p1) {
-            return String.fromCharCode('0x' + p1);
-        }));
-    }
+function mangoEncodeText(str) {
+    return str.replace(/[a-zA-Z]/g, function (c) {
+        return String.fromCharCode((c <= "Z" ? 90 : 122) >= (c = c.charCodeAt(0) + 13) ? c : c - 26);
+    });
+}
 
-    return encodeText(b64EncodeUnicode(str));
+function mangoB64EncodeUnicode(str) {
+    return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function (match, p1) {
+        return String.fromCharCode('0x' + p1);
+    }));
+}
+
+function mangoB64DecodeUnicode(str) {
+    return decodeURIComponent(Array.prototype.map.call(atob(str), function (c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
 }
 
 module.exports = mangoHash;
